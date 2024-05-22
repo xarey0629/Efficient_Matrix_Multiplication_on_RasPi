d0a698a447b74706336b3674e662686041801f12
xarey0629 <xarey0629@gmail.com>
2024-05-22 19:02:52 +0100

    Formatting all printing result. Comparation between simple multiplicaiton and CSR multiplication.

--- SpGEMM.c
+++ SpGEMM.c
@@ -5,9 +5,12 @@
 #define RATIO 0.25
 
 int randCnt = 0;
+double get_time();
+int SHOW = 0;
 
 void prepareSparse(float* matrix, int rows, int cols){
     int nnz = RATIO * rows * cols;
+    // printf("Number of non-zero elements: %d\n", nnz);
     srand((unsigned)time(NULL) + (randCnt++));
     for(int h = 0; h < nnz; h++) {
       int i = rand() % rows;
@@ -49,6 +52,7 @@ void CSRToMatrix(float* matrix, int rows,int cols, int* rowPtr, int* colInd, flo
 }
 
 void showMatrix(float* matrix, int rows, int cols){
+    if(SHOW == 0) return;
     printf("Matrix: \n");
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
@@ -60,6 +64,8 @@ void showMatrix(float* matrix, int rows, int cols){
 }
 
 void showCSR(int rows, int cols, int* rowPtr, int *colInd, float* val){
+    if(SHOW == 0) return;
+    
     printf("CSR: \n");
     printf("rowPtr: ");
     for(int i = 0; i < rows + 1; i++){
@@ -81,33 +87,89 @@ void showCSR(int rows, int cols, int* rowPtr, int *colInd, float* val){
     printf("\n\n");
 }
 
-void sparse_matrix_mul3(int rowPtr1[], int colInd1[], float val1[],
-                int rowPtr2[], int colInd2[], float val2[], 
-                float* matrixC, int M, int K, int N, int threads){ 
+// Input a pointer of a matrix and zeroize the array
+void zeroizeMatrix(float* matrix, int rows, int cols){
+    printf("******** Zeroize Matrix ********\n");
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            matrix[i * cols + j] = 0;
+        }
+    }
+}
 
+// Input two matrices and print the multiplication result
+void simpleMatrixMultiplication(float* matrixA, float* matrixB, float* matrixC, 
+                            int M, int K, int N){
+
+    printf("********** ********** **********\n");
+    printf("Simple Multiplicaiton: \n");
+    printf("- M x K x N = %d x %d x %d\n", M, N, M, K, N);
+    printf("- Number of non-zero elements in A: %d, in B: %d\n", (int)(M * K * RATIO), (int)(K * N * RATIO));
+    double start = get_time();
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            matrixC[i * N + j] = 0;
+            for(int k = 0; k < K; k++){
+                matrixC[i * N + j] += matrixA[i * K + k] * matrixB[k * N + j];
+            }
+        }
+    }
+    double end = get_time();
+    printf("- Time: %.3f\n", end - start);
+    if(SHOW)
+        showMatrix(matrixC, M, N);
+    printf("********** ********** **********\n");
+}
+
+
+void sparse_matrix_mul(int rowPtr1[], int colInd1[], float val1[],
+                int rowPtr2[], int colInd2[], float val2[], 
+                float* matrixC, int M, int K, int N){ 
+    
+    printf("********** ********** **********\n");
+    printf("Sparse Multiplication: \n");
+    printf("- M x K x N = %d x %d x %d\n", M, N, M, K, N);
+    printf("- Number of non-zero elements in A: %d, in B: %d\n", (int)(M * K * RATIO), (int)(K * N * RATIO));
+    double start = get_time();
     // #pragma omp parallel for num_threads(threads)
     for(int m = 0; m < M; m++){
         int row_start1 = rowPtr1[m];
         int row_end1 = rowPtr1[m + 1];
-        for(int idx = row_start1; idx < row_end1; idx++){
-            int k = colInd1[idx];
-            for(int t = rowPtr2[k]; t < rowPtr2[k + 1]; t++){
-                matrixC[m * N + colInd2[t]] += val1[idx] * val2[t];
+        for(int idx1 = row_start1; idx1 < row_end1; idx1++){
+            int col_1 = colInd1[idx1];            
+            for(int idx2 = rowPtr2[col_1]; idx2 < rowPtr2[col_1 + 1]; idx2++){
+                // Size of matrixC is M x N
+                matrixC[m * N + colInd2[idx2]] += val1[idx1] * val2[idx2];
             }
         }
     }
-    
+    double end = get_time();
+    printf("- Time: %.3f\n", end - start);
+    if(SHOW)
+        showMatrix(matrixC, M, N);
+    printf("********** ********** **********\n");
 }
 
-int main(int argc, char *argv[]){
-    // Input: M, K, N
-    // A: M x K
-    // B: K x N
-    // C: M x N
-    int M = strtol(argv[1], NULL, 10);
-    int K = strtol(argv[2], NULL, 10);
-    int N = strtol(argv[3], NULL, 10);
+// Get time in seconds
+double get_time(){
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (double)tv.tv_sec + (double)1e-6 * tv.tv_usec;
+}
 
+
+
+int main(int argc, char *argv[]){
+    // ************** Input(argv): M, K, N **************
+    int M = strtol(argv[1], NULL, 10);
+    int N = M, K = M;
+    // int K = strtol(argv[2], NULL, 10);
+    // int N = strtol(argv[3], NULL, 10);
+    
+    // ************** Multiplication ************** 
+    //            C   =    A    x    B
+    //          M x N = (M x K) x (K x N)
+    
     float *matrixA = (float*)malloc(M * K * sizeof(float));
     int *rowPtrA = (int*)malloc((M + 1) * sizeof(int));
     int *colIndA = (int*)malloc(M * K * RATIO * sizeof(int));
@@ -117,11 +179,12 @@ int main(int argc, char *argv[]){
     int *rowPtrB = (int*)malloc((K + 1) * sizeof(int));
     int *colIndB = (int*)malloc(K * N * RATIO * sizeof(int));
     float *valB = (float*)malloc(K * N * RATIO * sizeof(float));
-
+    
+    // Create random matrices
     prepareSparse(matrixA, M, K);
     prepareSparse(matrixB, K, N);
 
-    // Show matrix
+    // Display matrices
     showMatrix(matrixA, M, K);
     matrixToCSR(matrixA, M, K, rowPtrA, colIndA, valA);
     showCSR(M, K, rowPtrA, colIndA, valA);
@@ -129,6 +192,12 @@ int main(int argc, char *argv[]){
     showMatrix(matrixB, K, N);
     matrixToCSR(matrixB, K, N, rowPtrB, colIndB, valB);
     showCSR(K, N, rowPtrB, colIndB, valB);
+
+    // Create matrix C
+    float *matrixC = (float*)malloc(M * N * sizeof(float));
+    simpleMatrixMultiplication(matrixA, matrixB, matrixC, M, K, N);
+    zeroizeMatrix(matrixC, M, N);
+    sparse_matrix_mul(rowPtrA, colIndA, valA, rowPtrB, colIndB, valB, matrixC, M, K, N);
 
     return 0;
 }
